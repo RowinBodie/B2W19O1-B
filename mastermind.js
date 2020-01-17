39206b09ced8e7b0cd6136b0cbc494d55044a80b
Rowin Bodegom <99056991@mydavinci.nl>
2020-01-17 15:20:09 +0100

    laaste update en buggs eruit gehaald

--- mastermind.js
+++ mastermind.js
@@ -4,7 +4,13 @@ var colors=[
     "green",
     "red",
     "yellow",
-    "white"
+    "black"
+];
+var arrayColors = [
+    "first",
+    "second",
+    "third",
+    "fourth"
 ];
 
 var arrayBlock=[];
@@ -12,12 +18,16 @@ var arraySquircles=[];
 var arrayCircles=[];
 var theCode=[];
 var tempArray=[];
-var counter = 11;
+var next;
+var counter=11;
 var chosenOne;
+var answerColor=[];
+var playerChoice;
+var checkIfLost = false;
+var holdSelect;
 
 function generate(){
     var block;
-
     for(i=1;i<=12;i++){
         block = document.createElement("div");
         block.className = "block";
@@ -54,16 +64,28 @@ function generate(){
     div2.id = "answer";
     document.body.appendChild(div2);
 
+    var div3 = document.createElement("div");
+    div3.id = "answerText";
+    div3.innerHTML = "The Answer:";
+    document.body.appendChild(div3);
+
+    next = document.createElement("button");
+    next.id = "next";
+    next.innerHTML = "Next Game";
+    next.addEventListener("click", function(){reset()});
+    document.body.appendChild(next);
+
     for(j=0;j<4;j++){
         var circle = document.createElement("span");
         circle.className = "circles";
         document.getElementById("answer").appendChild(circle);
+        answerColor.push(circle);
     }
 
     var checkButton = document.createElement("button");
     checkButton.id = "check";
     checkButton.innerHTML = "Check";
-    checkButton.addEventListener("click", function(){check()});
+    checkButton.addEventListener("click", function(){checkForCheck()});
     document.body.appendChild(checkButton);
 
     var div = document.createElement("div");
@@ -73,38 +95,17 @@ function generate(){
     for(i=0;i<6;i++){
         var choice = document.createElement("span");
         choice.className = "circles";
-        if(i == 0){
-            choice.addEventListener("click", function(){changeColor("red")});
-            choice.style.background = "red";
-        }
-        if(i == 1){
-            choice.addEventListener("click", function(){changeColor("yellow")});
-            choice.style.background = "yellow";
-        }
-        if(i == 2){
-            choice.addEventListener("click", function(){changeColor("green")});
-            choice.style.background = "green";
-        }
-        if(i == 3){
-            choice.addEventListener("click", function(){changeColor("blue")});
-            choice.style.background = "blue";
-        }
-        if(i == 4){
-            choice.addEventListener("click", function(){changeColor("purple")});
-            choice.style.background = "purple";
-        }
-        if(i == 5){
-            choice.addEventListener("click", function(){changeColor("black")});
-            choice.style.background = "black";
-        }
+        choice.color = colors[i];
+        choice.style.background = colors[i];
+        choice.addEventListener("click", function(){changeColor(this.color)});
         document.getElementById("colorPallet").appendChild(choice);
     }
     console.log(arrayCircles);
     console.log(arraySquircles);
     console.log(arrayBlock);
 }
-
 function code(){
+    singleCode = [];
     for(i=0;i<4;i++){
         var number = Math.floor(Math.random() * 6);
         singleCode = colors[number];
@@ -112,53 +113,163 @@ function code(){
             var number = Math.floor(Math.random() * 6);
             singleCode = colors[number];
         }
-        theCode.push(singleCode)
+        theCode.push(singleCode);
+        console.log(theCode);
     }
-    start();
+    giveClick();
 }
-
-function start(){
-    var number1 = 0
-    var number2 = 1
-    var number3 = 2
-    var number4 = 3
+function giveClick(){
+    console.log(counter)
+    for(i=0;i<4;i++){
+        if(counter != -1){
+            var temp = arrayCircles[counter][i];
+            temp.number = i;
+            temp.addEventListener("click", function(){selected(this.number)});
+        }
+    }
+}
+function removeClick(){
     for(i=0;i<4;i++){
         var temp = arrayCircles[counter][i];
-        if(i == 0){
-            temp.addEventListener("click", function(){selected(number1)});
-        }
-        else if(i == 1){
-            temp.addEventListener("click", function(){selected(number2)});
-        }
-        else if(i == 2){
-            temp.addEventListener("click", function(){selected(number3)});
-        }
-        else if(i == 3){
-            temp.addEventListener("click", function(){selected(number4)});
-        }
+        temp.number = i;
+        temp.removeEventListener("click", function(){selected(this.number)});
     }
+    counter--
+    if(counter == -1){
+        lose();
+    }
+    giveClick();
 }
-
 function selected(select){
-    playerChoice = arrayCircles[counter][select];
+    holdSelect = select;
+    playerChoice = arrayCircles[counter][holdSelect];
     var border = document.getElementById("borderColor")
     if(border != null) border.id = "";
     playerChoice.id = "borderColor";
-    chosenOne = select;
+    chosenOne = holdSelect;
 }
-
-var playerChoice;
-var arrayColors = [
-    "first",
-    "second",
-    "third",
-    "fourth"
-];
-
 function changeColor(color){
     playerChoice.style.backgroundColor = color;
     arrayColors[chosenOne] = color;
+    if(holdSelect<3){
+        holdSelect+=1;
+    selected(holdSelect);
+    }
 }
-
+function checkForCheck(){
+    if(arrayColors[0] != "first"){
+        if(arrayColors[1] != "second"){
+            if(arrayColors[2] != "third"){
+                if(arrayColors[3] != "fourth"){
+                    check();
+                }
+            }
+        }
+    }
+}
+function check(){
+    var holdArray = [];
+    var progress = [];
+    for (i = 0; i < arrayColors.length; i++) {
+        holdArray[i] = arrayColors[i];
+    }
+    for(i=0;i<4;i++){
+        if(holdArray[i]==theCode[i]){
+            holdArray[i] = "rightP";
+            progress.push("r");
+        }
+    }
+    var holdCode = [];
+    for (i = 0; i < theCode.length; i++) {
+        holdCode[i] = theCode[i];
+    }
+    for(i=0;i<4;i++){
+        if(holdArray[i] != "rightP"){
+            for(n=0;n<4;n++){
+                if(holdArray[i]==theCode[n]){
+                    if(holdArray[n]!="rightP"&&holdCode[n]!="checked"){
+                        holdCode[n] = "checked";
+                        holdArray[i] = "wrongP";
+                        progress.push("w");
+                    }
+                }
+            }
+        }
+    }
+    checkProgress(progress);
+}
+function checkProgress(progress){
+    var tempProgress = progress;
+    for(i=0;i<tempProgress.length;i++){
+        var tempHoldSquircle = arraySquircles[counter][i];
+        if(tempProgress[i] == "r"){
+            tempHoldSquircle.style.backgroundColor = "red";
+        }
+        else if(tempProgress[i] =="w"){
+            tempHoldSquircle.style.backgroundColor = "white";
+        }
+    }
+    if(tempProgress[0] == "r"){
+        if(tempProgress[1] == "r"){
+            if(tempProgress[2] == "r"){
+                if(tempProgress[3] == "r"){
+                    victory()
+                }
+            }
+        }
+    }
+    arrayColors = [
+        "first",
+        "second",
+        "third",
+        "fourth"
+    ];
+    removeClick();
+    holdSelect = 0;
+    selected(holdSelect);
+}
+function showCode(){
+    for(i=0;i<4;i++){
+        var tempholdObj = answerColor[i];
+        var tempHoldC = theCode[i];
+        tempholdObj.style.backgroundColor = tempHoldC;
+    }
+}
+function victory(){
+    alert("You Won!");
+    showCode();
+    next.style.display = "inline-block";
+    checkIfLost = false;
+    removeClick();
+    selected(0);
+}
+function lose(){
+    alert("You Lose!");
+    showCode();
+    next.style.display = "inline-block";
+    checkIfLost = false;
+    removeClick();
+    selected(0);
+}
+function reset(){
+    for(i=0;i<4;i++){
+        var tempholdObj = answerColor[i];
+        tempholdObj.style.backgroundColor = "lavender";
+    }
+    for(i=11;i>=0;i--){
+        for(n=0;n<4;n++){
+            var holdCircle = arrayCircles[i][n];
+            var holdSquircle = arraySquircles[i][n];
+            holdCircle.style.backgroundColor = "lavender";
+            holdSquircle.style.backgroundColor = "";
+        }
+    }
+    checkIfLost = true;
+    counter = 11;
+    next.style.display = "none";
+    theCode=[];
+    code();
+}
 generate();
 code();
+selected(0);
